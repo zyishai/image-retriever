09b9f9b4538f063fd59c18feb0a28ab57ea53798
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-15 16:29:43 +0300

    moving multiprocess!

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 #! /Users/yishaizehavi/.virtualenvs/cv/bin/python3
 
+import multiprocessing as mp
 from correlogram import specific_correlogram
-from utils.dominant_colors import get_unique_colors
+from utils.colors import get_unique_colors
 from adapters.cv_image import OpenCVImage
 from utils.log import log_time
+
+PROCESSES = mp.cpu_count() - 1
 
 @log_time
 def main():
   luffy = OpenCVImage('imgs/luffy.jpg')
   colors = get_unique_colors(luffy)
 
-  for color in colors:
-    print(specific_correlogram(luffy, color, color, 8))
-    
-  luffy.show_simplified()
+  print(f'Running with {PROCESSES} processes!')
+  pool = mp.Pool(PROCESSES)
 
+  def cb(result):
+    for correlogram in result:
+      print(correlogram)
+
+  pool.starmap_async(specific_correlogram, map(lambda color: (luffy, color, color, 8), colors), callback=cb)
+    
+  pool.close()
+  pool.join()
 
 if __name__ == "__main__":
   main()
