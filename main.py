cc3b296ebdb9a46710bd10ac560b122737febca8
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-20 19:26:00 +0300

    test

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from utils.colors import get_unique_colors
 from adapters.cv_image import OpenCVImage
 from utils.log import log_time
 
+import cv2 as cv
+
 @log_time
 def main():
-  luffy = OpenCVImage('imgs/luffy.jpg')
+  luffy = OpenCVImage('imgs/luffy_small.jpg')
   colors = get_unique_colors(luffy)
-
+  
+  # This code runs for ~2.5 secs for `luffy_small.jpg` and for ~10.6 secs for `luffy.jpg`.
   for color in colors:
-    print(autocorrelogram(luffy, color, 8))
+    for i in range(1, 9):
+      print(autocorrelogram(luffy, color, i))
 
 if __name__ == "__main__":
   main()
