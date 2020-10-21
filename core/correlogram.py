fe811cddb132f4764dc558e90302ab82e0eb4ab6
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-21 12:14:16 +0300

    implemented correlogram contribution method

--- core/correlogram.py
+++ core/correlogram.py
@@ -2,7 +2,7 @@ from core.gamma import gamma
 
 # Eq. (3) & (11)
 def autocorrelogram(image, color, distance):
-  pixels = image.of_color(src_color)
+  pixels = image.of_color(color)
   numerator = gamma(pixels, distance)
   denominator = len(pixels) * 8 * distance
 
@@ -10,11 +10,11 @@ def autocorrelogram(image, color, distance):
 
 # Eq. (29)
 def local_autocorrelogram(image, color, distance):
-  pixels = image.of_color(src_color)
+  pixels = image.of_color(color)
   numerator = gamma(pixels, distance)
   denominator = 8 * distance
 
   return numerator / denominator
 
-def corr_contribution(sub_image, query_image):
-  pass
+def corr_contribution(sub_image, query_image, color):
+  return abs(autocorrelogram(query_image, color, 8) - local_autocorrelogram(sub_image, color, 8))
