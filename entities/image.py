3d28de92aeff1679a8a6680efd5a8187f65884fc
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-26 17:45:23 +0200

    removed useless code and optimized Image code

--- entities/image.py
+++ entities/image.py
@@ -1,23 +1,17 @@
-from entities.pixel import Pixel
-from numpy import where
+from numpy import where, asarray
 
 class Image:
   def __init__(self, img):
     self.source = img
-    (self.rows, self.columns) = self.source.shape
 
-  def size(self):
-    return (self.rows, self.columns)
-
-  def pixel_at(self, col, row) -> Pixel: # col=x, row=y
+  def pixel_at(self, col, row): # col=x, row=y
     if row >= len(self.source) or col >= len(self.source[0]) or row < 0 or col < 0:
       return None
-    return Pixel(self, col, row, self.source[row][col])
+    return self.source[row][col]
 
   def of_color(self, target_color):
     temp = where(self.source == target_color)
-    indices = zip(temp[1], temp[0]) # [(x1, y1) ... (xn, yn)]
-    return map(lambda index: Pixel(self, index[0], index[1], target_color), indices)
+    return asarray(temp).T # [(y1, x1) ... (yn, xn)]
 
   def flatten(self):
     if not hasattr(self, '_flat'):
