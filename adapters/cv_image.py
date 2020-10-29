17248dad58f5db4830fdd6f120c8084884bdeca3
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-29 13:03:05 +0200

    fixed `get_hist_sub_image` methods and cosmetics

--- adapters/cv_image.py
+++ adapters/cv_image.py
@@ -1,40 +1,53 @@
 from cv2 import imread, imshow, waitKey, destroyAllWindows
-from numpy import copy as np_copy, array as np_array, uint8, isnan
+from numpy import copy as np_copy, array as np_array, uint8
 from fast_histogram import histogram1d
 from entities.image import Image
 from utils.cluster import get_cluster, apply_cluster
 
 class OpenCVImage(Image):
   def __init__(self, path, cluster = None):
+    self.no_of_colors = 8
     self.cv_image = imread(path) # load image from disk.
 
     numeric_repr = self._set_cluster(cluster) # flat.
     
     super().__init__(numeric_repr.reshape(self.cv_image.shape[:-1])) # initialize parent class.
+    self.height, self.width = self.source.shape
 
     self._set_histogram() # calculate `histogram` property. 2d array.
 
   def _set_cluster(self, cluster=None):
     if cluster is None:
-      self.cluster = get_cluster(self.cv_image, 8)
+      self.cluster = get_cluster(self.cv_image, self.no_of_colors)
       return np_copy(self.cluster.labels_)
     else:
       self.cluster = cluster
       return apply_cluster(self.cv_image, self.cluster)
     
   def _set_histogram(self):
-    hist_values = histogram1d(self.source, bins=8, range=[0, 8])
+    hist_values = histogram1d(self.source, bins=self.no_of_colors, range=[0, self.no_of_colors])
     def to_hist_value(color):
       return hist_values[color]
 
     self.histogram = np_array(to_hist_value(self.source))
 
-  def get_hist_sub_image(self, offset, width, height):
-    x, y = offset
-    return self.histogram[y:y+height, x:x+width]
+  def dimensions(self):
+    if not hasattr(self, '_dim'):
+      self._dim = self.width, self.height
+    return self._dim
 
-  def get_sub_image(self, offset, width, height):
+  def get_hist_sub_image(self, offset, dimensions):
     x, y = offset
+    width, height = dimensions
+    sub = self.source[y:y+height, x:x+width]
+    hist_values = histogram1d(sub, bins=self.no_of_colors, range=[0, self.no_of_colors])
+    def to_hist_value(color):
+      return hist_values[color]
+    return np_array(to_hist_value(sub))
+
+  def get_sub_image(self, offset, dimensions):
+    x, y = offset
+    width, height = dimensions
     return Image(self.source[y:y+height, x:x+width])
 
   def show_simplified(self):
