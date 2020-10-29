ece9a0992f518c3a60c60eb764f111fe5b8a44d3
Yishai Zehavi <zehaviyishai@gmail.com>
2020-10-29 13:05:08 +0200

    fixed contribution error and added memoization to improve performance

--- core/correlogram.py
+++ core/correlogram.py
@@ -1,20 +1,33 @@
+from utils.image import get_unique_colors
 from core.gamma import gamma
+from utils.decorators import memoize
 
 # Eq. (3) & (11)
-def autocorrelogram(image, color, distance):
+@memoize
+def autocorrelogram(image, color, distance_set):
   pixels = image.of_color(color) # (y, x)
-  numerator = gamma(pixels, distance)
-  denominator = len(pixels) * 8 * distance
+  if len(pixels) == 0:
+    return 0
+  numerator = gamma(pixels, distance_set)
+  denominator = len(pixels) * 8 * sum(distance_set)
 
   return numerator / denominator
 
 # Eq. (29)
-def local_autocorrelogram(image, color, distance):
+def local_autocorrelogram(image, color, distance_set):
   pixels = image.of_color(color) # (y, x)
-  numerator = gamma(pixels, distance)
-  denominator = 8 * distance
+  if len(pixels) == 0:
+    return 0
+  numerator = gamma(pixels, distance_set)
+  denominator = 8 * sum(distance_set)
 
   return numerator / denominator
 
-def corr_contribution(sub_image, query_image, color):
-  return abs(autocorrelogram(query_image, color, 8) - local_autocorrelogram(sub_image, color, 8))
+def corr_contribution(sub_image, query_image, distance_set=(1, 3, 5)):
+  result = 0;
+  colors = get_unique_colors(sub_image)
+  for color in colors:
+    a = autocorrelogram(query_image, color, distance_set)
+    b = local_autocorrelogram(sub_image, color, distance_set)
+    result += (a - b)
+  return result
