62f725c3a17edacb67148d9d98e9362d7e51313c
Yishai Zehavi <zehaviyishai@gmail.com>
2020-11-01 17:22:39 +0200

    reduced gamma run time by ~55%

--- core/gamma.py
+++ core/gamma.py
@@ -1,6 +1,7 @@
-from numpy import concatenate, where, maximum, isin
+from scipy.spatial.distance import cdist
+from numpy import isin
 
 # Total running complexity O(4D(N^2)) -> O(D(N^2)).
 def gamma(pixels, distance_set):
-  dist = concatenate(list(map(lambda pixel: abs(pixels - pixel), pixels)))
-  return len(where(isin(maximum(dist[:, 0], dist[:, 1]), distance_set))[0])
+  distances = cdist(pixels, pixels, metric='chebyshev')
+  return len(distances[isin(distances, distance_set)])
